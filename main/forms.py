e77320a5f7891c3c0c283cafda71e7384b7f1a12
Himanshu Mamgain <mamgainjii@gmail.com>
2023-03-15 00:02:00 +0530

    styling registration form

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 # from main.models import Blog
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
     
@@ -35,21 +35,29 @@ class RegisterForm(forms.ModelForm):
 
 class CustomUserCreationForm(UserCreationForm):
 
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form form-control'}), max_length=30)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form form-control'}),max_length=30)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form form-control'}),label="Password",max_length=30)
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form form-control'}),label="Confirm Password",max_length=30)
+
+
     class Meta:
         model = User
-        fields = ('username','password1','password2')
+        fields = ('username','email' ,'first_name', 'last_name')
+
+        help_texts = {
+            'username': "",
+            'password1': ""
+        }
 
         
 
-        labels = {
-            'username': "",
-            'password1': "",
-            'password2': "",
+        widgets = {
+            'username': forms.TextInput(attrs={
+            'class': 'form form-control',
+            }),
+            
         }
 
-        help_texts = {
-            'username': None,
-            'password1': None,
-            'password2': None,
-        }
-
+        
