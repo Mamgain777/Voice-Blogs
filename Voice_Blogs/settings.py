4932cbc71c11306ba6dcba2ccaade1cfffdff6ae
Himanshu Mamgain <mamgainjii@gmail.com>
2023-03-20 11:08:49 +0530

    trying optimization

--- Voice_Blogs/settings.py
+++ Voice_Blogs/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = Path.joinpath(BASE_DIR,'staticfiles')
 
 # setting up media directory
 MEDIA_URL = "media/"
